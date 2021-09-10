3aaf0ca4e9bff132a737c7c4ada0d5b9d2f0a601
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-10 13:41:32 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,6 +1,6 @@
 <script>
-  https://avatars.dicebear.com/api/:sprites/:seed.svg
-  https://www.boredapi.com/api/activity
+  #https://avatars.dicebear.com/api/:sprites/:seed.svg
+  #https://www.boredapi.com/api/activity
   $("p").css("background-color","blue");
   $("h1").text("Dogs!");
 
