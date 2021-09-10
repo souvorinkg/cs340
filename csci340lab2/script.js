978ca2cac24ce741f1798a5755b449c6256bb239
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-10 13:33:31 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,6 +1,6 @@
 <script>
   https://avatars.dicebear.com/api/:sprites/:seed.svg
   https://www.boredapi.com/api/activity
-  $("h1").css("color","blue");
+  $("p").css("background-color","blue");
 
 </script>
