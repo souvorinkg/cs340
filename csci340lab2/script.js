c634380960ba42c7be599fd110ec0dc7700df573
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-10 13:50:18 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,5 +1,12 @@
-
-  //https://avatars.dicebear.com/api/:sprites/:seed.svg
-  //https://www.boredapi.com/api/activity
+$( document ).ready(function() {
+  console.log( "ready!" );
   $("p").css("background-color","blue");
   $("h1").text("Dogs!");
+  $('<button/>', {
+        text: Generate,
+        id: avatarButton,
+        click: function () { alert('hi'); }
+});  
+//https://avatars.dicebear.com/api/:sprites/:seed.svg
+  //https://www.boredapi.com/api/activity
+  
