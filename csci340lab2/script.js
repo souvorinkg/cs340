5c54c23f5b6f6989784e56d77c151d021d33d3e9
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-11 17:01:23 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,14 +1,8 @@
 $( document ).ready(function() {
   console.log( "ready!" );
-  $("p").css("background-color","blue");
-  $("h1").text("Dogs!");
-  $('<button/>', {
-        text: Generate,
-        id: avatarButton,
-        click: function () { alert('hi'); console.log("button works!")}
-  }); 
-  $("#avatarButton").appendTo("#p1");
+  $("#avatarImg").on("click", function() {
+    $("#avatarImg").attr("src", "https://avatars.dicebear.com/api/:sprites/:seed.svg");
 });
-//https://avatars.dicebear.com/api/:sprites/:seed.svg
+//
   //https://www.boredapi.com/api/activity
   
