505f93514ba06f08c99d8f83c00858bd1a967337
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 10:25:49 -0500

    Update script.js
    try abstracting link text

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -2,8 +2,10 @@ $( document ).ready(function() {
   console.log( "ready!" );
   $("#avatarImg").on("click", function(event) {
             console.log(event);
+            var $linkText = $("<text>")
+                $linkText.html("https:\/\/randomfox.ca\/?i="+"88")
             var $img = $("<img>")
-                $("#avatarImg").attr("src","https:\/\/randomfox.ca\/?i=88")
+                $("#avatarImg").attr("src",linkText)
                 i++
         });
 });
