48926fc65e9627ffcd34e5bd36142f7a57b1858c
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 09:38:09 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   console.log( "ready!" );
   $("#avatarImg").on("click", function() {
-    $("#avatarImg").attr("src", "https://avatars.dicebear.com/api/:sprites/:seed.svg")};
+    $("#avatarImg").attr("src", "https://avatars.dicebear.com/api/:sprites/:seed.svg")});
 });
   //https://www.boredapi.com/api/activity
   
