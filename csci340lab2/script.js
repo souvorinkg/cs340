a4f37b1304c91f2a6d0f79de2156271cfaea4cce
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 09:56:05 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,7 +1,15 @@
 $( document ).ready(function() {
   console.log( "ready!" );
-  $("#avatarImg").on("click", function() {
-    $("#avatarImg").attr("src", "https://avatars.dicebear.com/api/:sprites/:seed.svg")});
+  $("body").on("click", function(event) {
+            console.log(event);
+            var $img = $("<img>")
+                .attr("src", "https://www.kasandbox.org/programming-images/animals/crocodiles.png")
+                .width(100)
+                .css("position", "absolute")
+                .css("top", event.pageY + "px")
+                .css("left", event.pageX + "px")
+                .appendTo("body");
+        });
 });
   //https://www.boredapi.com/api/activity
   
