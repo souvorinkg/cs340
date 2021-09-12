e1c771a6236bdc14a940e473e7afd186df06fd56
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 10:02:10 -0500

    Update script.js
    Move from khan code to my code

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -3,12 +3,6 @@ $( document ).ready(function() {
   $("#avatarImg").on("click", function(event) {
             console.log(event);
             var $img = $("<img>")
-                .attr("src", "https://www.kasandbox.org/programming-images/animals/crocodiles.png")
-                .width(100)
-                .css("position", "absolute")
-                .css("top", event.pageY + "px")
-                .css("left", event.pageX + "px")
-                .appendTo("body");
                 $("#avatarImg").attr("src","https://www.kasandbox.org/programming-images/animals/crocodiles.png")
         });
 });
