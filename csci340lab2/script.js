2d581f3f0b19d67828b1b0e82422913fe0aca92b
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 11:40:43 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -4,11 +4,11 @@ $.ajax({
     url: "https://random.dog/woof.json",
     success: function(results) {
       console.log(results["url"]);
-      if (results["url"].endsWith(".mp4")) {
-        $('#dog').attr("src", "images/blank.png");
-      } else {
-        $('#dog').attr("src", results["url"]);
-      }
+      //if (results["url"].endsWith(".mp4")) {
+        //$('#dog').attr("src", "images/blank.png");
+      //} else {
+        //$('#dog').attr("src", results["url"]);
+      //}
     },
     error: function(xhr,status,error) {
       console.log(error);
