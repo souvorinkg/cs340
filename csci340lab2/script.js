6913ed6156ab183e28c4159aa68526a41bdf0b85
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 10:18:57 -0500

    Update script.js
    Link instead of image

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   $("#avatarImg").on("click", function(event) {
             console.log(event);
             var $img = $("<img>")
-                $("#avatarImg").attr("src","https:\/\/randomfox.ca\/images\/104.jpg")
+                $("#avatarImg").attr("src","https:\/\/randomfox.ca\/?i=88")
         });
 });
   //https://www.boredapi.com/api/activity
