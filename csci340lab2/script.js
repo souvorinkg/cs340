7771a6fcd41235a32436231ecad872fc06d57619
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 10:48:27 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,7 +1,7 @@
   $("#avatarImg").on("click", function(event) {
             console.log(event);
             var $img = $("<img>")
-                $("#avatarImg").attr("src","https:\/\/randomfox.ca\/?i=88")
+                $("#avatarImg").attr("src","https:\/\/randomfox.ca\/images\/113.jpg")
 });
   //https://www.boredapi.com/api/activity
   
