7f44500d9f19b05eb3b3a68ae0370193be687d51
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 11:23:15 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,4 +1,7 @@
-  $("#avatarImg").on("click", function(event) {
+$.getJSON("https://randomfox.ca/floof/?ref=apilist.fun", function(data) {
+  console.log(data);
+});
+$("#avatarImg").on("click", function(event) {
             console.log(event);
             var $img = $("<img>")
                 $("#avatarImg").attr("src","https:\/\/randomfox.ca\/images\/113.jpg")
