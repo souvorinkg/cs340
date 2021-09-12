4096068fc950595b1273eae65a1467fc99d366ca
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 10:27:26 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
   console.log( "ready!" );
+  var $linkText = $("<text>")
+                $linkText.html("https:\/\/randomfox.ca\/?i="+"88")
   $("#avatarImg").on("click", function(event) {
             console.log(event);
-            var $linkText = $("<text>")
-                $linkText.html("https:\/\/randomfox.ca\/?i="+"88")
+            
             var $img = $("<img>")
-                $("#avatarImg").attr("src",linkText)
+                $("#avatarImg").attr("src",$linkText)
         });
 });
   //https://www.boredapi.com/api/activity
