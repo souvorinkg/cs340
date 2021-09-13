b2c04b30217ac2d7790ac09c778e3a27854201d8
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 20:49:47 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,5 +1,28 @@
-
-$('#pActivity').click(function() {
+$(document).ready(function() {  
+    $('#pActivity').click(function() {
+        dataType: "json",
+        url: "https://dog.ceo/api/breeds/image/random",
+        success: function(message) {
+          console.log(message["url"]);
+          console.log(url);
+          $("#avatarImg").on("click", function(event) {
+                console.log(event);
+                var $img = $("<img>")
+                    $("#avatarImg").attr("src",message["url"])
+                    $("#avatarImg").css("width","250")
+          })
+          //if (results["url"].endsWith(".mp4")) {
+            //$('#avatarImg').attr("src", "images/blank.png");
+          //} else {
+            //$('#avatarImg').attr("src", results["url"]);
+          //}
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        console.log(results);
+        }
+    }
+    $('#pActivity').click(function() {
     $.ajax({
       dataType: "jsonp",
       url: "https://www.boredapi.com/api/activity",
@@ -10,6 +33,7 @@ $('#pActivity').click(function() {
         console.log(error);
       }
     });
-  }); 
-  //
+  });                                              
+})
+
   
