0cab5b4f4825d06b36c7d4dc900add18e7534cfb
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 20:09:19 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -16,15 +16,12 @@ $.ajax({
         //$('#avatarImg').attr("src", results["url"]);
       //}
     //},
-    error: function(xhr,status,error) {
-      console.log(error);
-    }
 });
 $('#pActivity').click(function() {
     $.ajax({
       dataType: "jsonp",
-      url: "https://www.boredapi.com/api/activity/?method=getActivity&format=jsonp&lang=en&jsonp=parseQuote",
-      success: function(results) {
+      url: "https://www.boredapi.com/api/activity",
+      success: function() {
         $('#pActivity').text("http://www.boredapi.com/api/activity?key=5881028");
       },
       error: function(xhr,status,error) {
