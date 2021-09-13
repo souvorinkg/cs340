537a21e6ab54835a70d8d3b1bd6cef7b8233e10d
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-12 20:11:22 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,22 +1,3 @@
-
-$.ajax({
-    dataType: "json",
-    message: "https://dog.ceo/api/breeds/image/random",
-    success: function(message) {
-      console.log(message["url"]);
-      $("#avatarImg").on("click", function(event) {
-            console.log(event);
-            var $img = $("<img>")
-                $("#avatarImg").attr("src",message["url"])
-                $("#avatarImg").css("width","250")
-      })
-      //if (results["url"].endsWith(".mp4")) {
-        //$('#avatarImg').attr("src", "images/blank.png");
-      //} else {
-        //$('#avatarImg').attr("src", results["url"]);
-      //}
-    //},
-});
 $('#pActivity').click(function() {
     $.ajax({
       dataType: "jsonp",
