001e4946570f4cec0c4ee1a3923a5bdf7c3cc247
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-16 13:14:32 -0500

    Update script.js
    Changed p activity

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,21 +1,11 @@
 $(document).ready(function() {  
     $('#pActivity').click(function() {
         dataType: "json",
-        url: "https://dog.ceo/api/breeds/image/random",
+        url: "https://www.boredapi.com/api/activity",
         success: function(message) {
-          console.log(message["url"]);
-          console.log(url);
-          $("#avatarImg").on("click", function(event) {
-                console.log(event);
-                var $img = $("<img>")
-                    $("#avatarImg").attr("src",message["url"])
-                    $("#avatarImg").css("width","250")
-          })
-          //if (results["url"].endsWith(".mp4")) {
-            //$('#avatarImg').attr("src", "images/blank.png");
-          //} else {
-            //$('#avatarImg').attr("src", results["url"]);
-          //}
+          console.log(message["activity"]);
+          console.log(activity);
+          $("#pActivity").text(message["activity"]);
         },
         error: function(xhr,status,error) {
           console.log(error);
