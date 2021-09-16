7b3bd1ed0afb94eb21a74bc480bd0ae8e584aac6
souvorinkg <89923856+souvorinkg@users.noreply.github.com>
2021-09-16 13:18:32 -0500

    Update script.js
    Fixed syntax

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,29 +1,21 @@
 $(document).ready(function() {  
     $('#pActivity').click(function() {
-        dataType: "json",
-        url: "https://www.boredapi.com/api/activity",
-        success: function(message) {
-          console.log(message["activity"]);
-          console.log(activity);
-          $("#pActivity").text(message["activity"]);
-        },
-        error: function(xhr,status,error) {
-          console.log(error);
-        console.log(results);
-        }
-    }
-    $('#pActivity').click(function() {
-    $.ajax({
-      dataType: "jsonp",
-      url: "https://www.boredapi.com/api/activity",
-      success: function() {
-        $('#pActivity').text("http://www.boredapi.com/api/activity?key=5881028");
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
-      }
+        $.ajax({
+            dataType: "json",
+            url: "https://www.boredapi.com/api/activity",
+            success: function(message) {
+              console.log(message["activity"]);
+              console.log(activity);
+              $("#pActivity").text(message["activity"]);
+            },
+            error: function(xhr,status,error) {
+              console.log(error);
+            console.log(results);
+            }
+        });
     });
-  });                                              
-})
+});
+
+                          
 
   
